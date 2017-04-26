3e9802505d02d9d010b3d242f4b0616d7dbf6bd6
jeremy <kepung@gmail.com>
2017-04-26 13:16:11 +1200

    personservice  add and search.

--- AgeRanger/wwwroot/src/app/services/PersonService.spec.ts
+++ AgeRanger/wwwroot/src/app/services/PersonService.spec.ts
@@ -15,11 +15,9 @@ import { Person } from '../Person';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/toPromise';
-
 import { PersonService } from './PersonService';
 
 const fakePersonData = () => [
@@ -96,24 +94,107 @@ describe('PersonService List', () => {
             .toPromise();
     })));
 
-     it('listing ok with no results', async(inject([], () => {
-         let okResponse = new Response(new ResponseOptions({status: 200, body: {data : []}}));
-         backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
-         service.listPerson().do(plist => {
-             expect(plist.length).toBe(0, 'no person in list is ok too')
-         })
+    it('listing ok with no results', async(inject([], () => {
+        let okResponse = new Response(new ResponseOptions({ status: 200, body: { data: [] } }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+        service.listPerson().do(plist => {
+            expect(plist.length).toBe(0, 'no person in list is ok too')
+        })
+    })));
 
-     })));
+    it('500 - error received', async(inject([], () => {
+        let okResponse = new Response(new ResponseOptions({
+            status: 500
+        }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+        service.listPerson().do(plist => {
 
-    // it('500 server error ', async(inject[], () => {
-    //     let okResponse = new Response(new ResponseOptions({
-    //         status: 500
-    //     }));
-    //     backend.connections.subscribe((c: MockConnection) => c.mockResponse(response));
-    //     service.listPerson().do(plist => {
-    //         fail('should be no person');
-    //     }).catch(err => {
-    //         return Observable.of(null);
-    //     });
-    // }));
+            fail('should be no person');
+        }).catch(err => {
+            return Observable.of(null);
+        });
+    })));
+});
+
+describe('PersonService Search', () => {
+
+    let backend: MockBackend;
+    let service: PersonService;
+    let personData: Person[];
+    let response: Response;
+
+    const fakePersonData = () => [
+        { id: 1, firstName: 'Jermaine', lastName: 'Nelson', age: 4, ageGroup: 'Child' },
+        { id: 2, firstName: 'Phil', lastName: 'McCurdy', age: 20, ageGroup: 'Teenager' },
+        { id: 3, firstName: 'Shannon', lastName: 'Alexander', age: 11, ageGroup: 'Child' },
+        { id: 4, firstName: 'Duncan', lastName: 'Tim', age: 50, ageGroup: 'Kinda old' }
+    ] as Person[];
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule],
+            providers: [
+                PersonService,
+                { provide: XHRBackend, useClass: MockBackend }
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+
+        backend = be;
+        service = new PersonService(http);
+        personData = fakePersonData();
+        let options = new ResponseOptions({ status: 200, body: { data: personData } });
+        response = new Response(options);
+
+    }));
+
+    it('firstname, lastname null search', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+        service.search(null, null)
+            .do(plist => {
+                console.log(plist);
+                expect(plist.data.length).toBe(personData.length,
+                    'should have expected no. of Person');
+            })
+            .toPromise();
+    })));
+});
+
+describe('PersonService Add', () => {
+
+    let backend: MockBackend;
+    let service: PersonService;
+    let personData: Person[];
+    let response: Response;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule],
+            providers: [
+                PersonService,
+                { provide: XHRBackend, useClass: MockBackend }
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+
+        backend = be;
+        service = new PersonService(http);
+        let options = new ResponseOptions({ status: 201, body: {} });
+        response = new Response(options);
+
+    }));
+
+    it('person add ok response', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+        service.addPerson(new Person({ firstName: 'john', lastName: 'doe' }))
+            .do(result => {
+                expect(result.status).toBe(201);
+                expect(result.ok).toBe(true);
+            })
+            .toPromise();
+    })));
 });
